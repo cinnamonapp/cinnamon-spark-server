55949ddd48f9aa3d7007712dde52c2ad3cbf817e
Alessio Santo <alessio.santocs@gmail.com>
2015-04-14 11:35:37 +0200

    Meals api improved

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,8 +1,9 @@
 class Meal
   attr_accessor :created_at, :meal_records
 
-  def initialize
-    self.meal_records = []
+  def initialize(params={})
+    self.created_at = params[:created_at] || nil
+    self.meal_records = params[:meal_records] || []
   end
 end
 
@@ -13,33 +14,36 @@ class MealsController < ApplicationController
     # Prepare an array of meals
     @meals = [] # [Meal]
 
-    # For each +1 meal records
     meal_records = MealRecord.all
     meal_records = @user.meal_records if @user
+    meal_records = meal_records.order(:created_at => :desc)
 
+    # For each +1 meal records
     meal_records.each_with_index do |meal_record, index|
       if index > 0
         previous_meal_record = meal_records[index - 1]
 
         # if the creation date is within an hour after the creation date of the previous meal
-        if meal_record.created_at - previous_meal_record.created_at <= 30.minutes
+        if (meal_record.created_at - previous_meal_record.created_at).abs <= 30.minutes
           # add it to the same group of the previous meal
           meal = @meals.last
           meal.meal_records.push meal_record
         else
           # create a new group
-          meal = Meal.new()
-          meal.created_at = meal_record.created_at
-          meal.meal_records.push meal_record
+          meal = Meal.new(
+            :created_at => meal_record.created_at,
+            :meal_records => [meal_record]
+          )
 
           @meals.push meal
         end
 
       else
         # Initialize the first Meal!
-        meal = Meal.new()
-        meal.created_at = meal_record.created_at
-        meal.meal_records.push meal_record
+        meal = Meal.new(
+          :created_at => meal_record.created_at,
+          :meal_records => [meal_record]
+        )
 
         @meals.push meal
 
--- app/views/meals/index.json.jbuilder
+++ app/views/meals/index.json.jbuilder
@@ -1,1 +1,4 @@
-json.array! @meals, :created_at, :meal_records
+json.array! @meals do |meal|
+  json.extract! meal, :created_at
+  json.meal_records meal.meal_records, partial: "meal_records/show", as: :meal_record
+end
