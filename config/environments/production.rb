bbff45e61a50860778bde9dbb238a01542fe3686
Alessio Santo <alessio.santocs@gmail.com>
2015-04-16 15:31:26 +0200

    Adding mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,17 @@ CinnamonSparkServer::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.zoho.com',
+    :port           => '465',
+    :authentication => :plain,
+    :user_name      => 'alessio@cinnamon-app.com',
+    :password       => 'snaphealth',
+    :domain         => 'cinnamon-app.com'
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
