3008fddfe080db4eb007203c83beeecd889f382b
Alessio Santo <alessio.santocs@gmail.com>
2015-04-16 17:02:08 +0200

    Added amazon ses file

---
+++ config/initializers/amazon_ses.rb
@@ -0,0 +1,3 @@
+ActionMailer::Base.add_delivery_method :ses_system_message, AWS::SES::Base,
+                                       :access_key_id     => ENV['MAILER_USERNAME'],
+                                       :secret_access_key => ENV['MAILER_PASSWORD']
