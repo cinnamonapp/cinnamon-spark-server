79292c42bba0b25adef00aa89b5bf406a2f7414e
Alessio Santo <alessio.santocs@gmail.com>
2015-04-17 07:19:58 +0200

    Trying with another address

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,4 @@
 ActionMailer::Base.add_delivery_method :ses_system_message, AWS::SES::Base,
                                        :access_key_id     => ENV['MAILER_USERNAME'],
                                        :secret_access_key => ENV['MAILER_PASSWORD'],
-                                       :server => 'email.eu-west-2.amazonaws.com'
+                                       :server => 'email.us-west-2.amazonaws.com'
